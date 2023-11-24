2c6891a9c6a7bdb92a2fcbcb0a6a746011de250f
Omvpatil <120188097+Omvpatil@users.noreply.github.com>
2023-11-24 21:38:03 +0530

    Update task3.py

--- Task_3/task3.py
+++ Task_3/task3.py
@@ -4,15 +4,20 @@ import simple_colors
 Capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 Small = "abcdefghijklmnopqrstuvwxyz"
 Special = "~`@!#$%^&*()_+{}:;/?"
-Num = "1234567890"
+num = "1234567890"
 password = ""
-
 char = [Capital, Special, Small, num]
 length = int(input(simple_colors.yellow("Enter length of password :")))
-
 for i in range(length):
     for j in random.choices(char,):
         password += random.choice(j)
         break
 
 print(simple_colors.green("Password :"), simple_colors.red(password))
+print(simple_colors.magenta("Save Password ?"))
+choice = input("[Y]es/[N]o :")
+if choice == "y" "Y":
+    choice = input(simple_colors.green("By which name ?\n"))
+    f = open("password.txt", "a")
+    f.write(choice + "-->" + password)
+    f.close()
